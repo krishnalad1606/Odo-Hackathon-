5023376858802f05b879879488963c540788afb0
krishnalad1606 <krishnalad1606@gmail.com>
2026-08-22 12:28:52 +0530

    Initial project setup

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const notes = []
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body);
+
+    res.status(201).json({
+        message: 'Note created successfully'    
+    })
+})
+
+module.exports = app;
+
+
+
