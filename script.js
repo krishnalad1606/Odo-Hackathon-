7bb20382b78ac84139ce8e13724ee64d2fef9581
rachite3731s-bit <rachite3731s@gmail.com>
2026-08-22 13:36:17 +0530

    add js in hero section

---
+++ script.js
@@ -0,0 +1,21 @@
+// ================= HERO BUTTONS =================
+
+function planTrip() {
+    openSignup();
+}
+
+
+function exploreDestinations() {
+    document
+        .getElementById("destinations")
+        .scrollIntoView({
+            behavior: "smooth"
+        });
+}
+
+
+function viewAll() {
+    alert("Destination explorer coming soon!");
+}
+
+
